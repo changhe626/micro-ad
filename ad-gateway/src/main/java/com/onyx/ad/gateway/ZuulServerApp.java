5c9fc16f0d5bfe278895012ec9d52d3a4f4657bf
zhangke <zhangke@curefun.com>
2019-09-25 10:09:38 +0800

    修复了zuul请求转发的bug 2019年9月25日10:09:27

--- ad-gateway/src/main/java/com/onyx/ad/gateway/ZuulServerApp.java
+++ ad-gateway/src/main/java/com/onyx/ad/gateway/ZuulServerApp.java
@@ -2,11 +2,20 @@ package com.onyx.ad.gateway;
 
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringCloudApplication
-@EnableZuulServer
+/**
+ * 这里是EnableZuulProxy   不是 @EnableZuulServer 注解, 记住了
+ */
+//@SpringCloudApplication
+//@EnableZuulServer
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+@EnableCircuitBreaker
 public class ZuulServerApp {
 
     public static void main(String[] args) {
